4c1bd1d43b3ec18aec8abb308b13001884a13a50
Noah Litvin <noah.litvin@gmail.com>
2023-03-06 23:10:54 -0500

    add cannonfile

---
+++ cannonfile-update.sh
@@ -0,0 +1,11 @@
+cannon alter weth:9 set-url ipfs://QmevANnVi9bmMjqZdBfwEGTscWLdJBLqoa8ikepVHsTpyj --chain-id 1
+cannon alter weth:9 set-contract-address WETH9 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2 --chain-id 1
+
+cannon alter weth:9 set-url ipfs://QmevANnVi9bmMjqZdBfwEGTscWLdJBLqoa8ikepVHsTpyj --chain-id 5
+cannon alter weth:9 set-contract-address WETH9 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6 --chain-id 5
+
+cannon alter weth:9 set-url ipfs://QmevANnVi9bmMjqZdBfwEGTscWLdJBLqoa8ikepVHsTpyj --chain-id 10
+cannon alter weth:9 set-contract-address WETH9 0x4200000000000000000000000000000000000006 --chain-id 10
+
+cannon alter weth:9 set-url ipfs://QmevANnVi9bmMjqZdBfwEGTscWLdJBLqoa8ikepVHsTpyj --chain-id 42161
+cannon alter weth:9 set-contract-address WETH9 0x82af49447d8a07e3bd95bd0d56f35241523fbab1 --chain-id 42161
